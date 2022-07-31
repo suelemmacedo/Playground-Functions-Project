00fcce265251e81176d6e020986ea9c811c27f1c
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-07-31 13:52:50 -0300

    Concluindo requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -88,11 +88,11 @@ return string;
 }
 
 // Desafio 9
-function encode() {
-  
+function encode(string) {
+ return string.replaceAll("a","1").replaceAll("e", "2").replaceAll("i","3").replaceAll("o", "4").replaceAll("u","5"); 
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  return string.replaceAll("1","a").replaceAll("2", "e").replaceAll("3","i").replaceAll("4", "o").replaceAll("5","u"); 
 }
 
 // Desafio 10
